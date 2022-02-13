4463258897fba9093b91bd528515fc4c19f6ba7b
Duygu Temur <duygutemur0@gmail.com>
2022-02-13 21:32:58 +0300

    add status messages

---
+++ src/components/StatusMessage.jsx
@@ -0,0 +1,16 @@
+
+const StatusMessage = ({ winner, current }) => {
+
+    const noMovesLeft = current.board.every(el => el !== null )
+
+    return (
+        <h2>
+            {winner && `winner is ${winner}`}
+            {!winner && !noMovesLeft && `next player is ${current.isXNext ? 'X' : 'O'} `}
+            {!winner && noMovesLeft && 'X and O tied'}
+        </h2>
+    )
+
+}
+
+export default StatusMessage;
